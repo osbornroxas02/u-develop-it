1a368d2a98577bf0a877a8373561347a7e275dca
osbornroxas <osbornroxas02@gmail.com>
2020-08-10 03:00:01 -0700

    voters

--- db/schema.sql
+++ db/schema.sql
@@ -15,3 +15,10 @@ CREATE TABLE candidates (
     DELETE
     SET NULL
 );
+CREATE TABLE voters (
+    id INTEGER PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
